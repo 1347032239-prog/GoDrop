23cb48f02ed5545f1b330f15466b53b1dafe8043
xantarescpp <1347032239@qq.com>
2026-08-29 14:48:24 +0800

    feat: recognize scan command

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
-	fmt.Println("GoDrop")
+
+	argCount := len(os.Args)
+	if argCount > 1 {
+		if os.Args[1] == "scan" {
+			fmt.Println("准备扫描文件")
+		}
+		else {
+			fmt.Println("请输入正确的参数！")
+		}
+	}
+
+	fmt.Println("用法: GoDrop scan")
 
 }
