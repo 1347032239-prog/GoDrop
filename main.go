0d8e6730d9280e8ecb57b495a947c19583ecb5ad
xantarescpp <1347032239@qq.com>
2026-08-29 15:38:59 +0800

    feat: parse scan dictionary flag

--- main.go
+++ main.go
@@ -2,15 +2,29 @@ package main
 
 import "fmt"
 import "os"
+import "flag"
+import "path/filepath"
 
 func main() {
 
 	argCount := len(os.Args)
 	if argCount > 1 {
 		if os.Args[1] == "scan" {
-			fmt.Println("准备扫描文件")
+			if argCount == 2 {
+				fmt.Println("错误：必须使用 -dir 指定扫描目录")
+			} else {
+				scanCmd := flag.NewFlagSet("scan", flag.ContinueOnError)
+				dirPtr := scanCmd.String("dir", ".", "目标路径")
+				err := scanCmd.Parse(os.Args[2:])
+				if err != nil {
+
+				}
+				cleanPath := filepath.Clean(*dirPtr)
+
+				fmt.Printf("准备扫描目录:%v\n", cleanPath)
+			}
 		} else {
-			fmt.Println("请输入正确的参数！")
+			fmt.Println("请输入正确的功能参数！")
 		}
 
 		return
