f37bd8bfcd220bdf6bbd8d647c812470def442e6
xantarescpp <1347032239@qq.com>
2026-08-31 20:57:00 +0800

    recursively scan regular files

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "os"
 import "flag"
 import "path/filepath"
 import "errors"
+import "io/fs"
 
 func main() {
 
@@ -22,9 +23,9 @@ func main() {
 				fmt.Println("必须使用 -dir 指定扫描目录， 可能未传入路径")
 				return
 			}
-			if NArg := scanCmd.NArg(); NArg > 0 {
+			if nArg := scanCmd.NArg(); nArg > 0 {
 				fmt.Print("错误: 不支持多余参数:")
-				for i := 0; i < NArg; i++ {
+				for i := 0; i < nArg; i++ {
 					fmt.Printf("%v ", scanCmd.Arg(i))
 				}
 				return
@@ -39,12 +40,42 @@ func main() {
 				fmt.Printf("文件存在仍错误, 错误信息:%v\n", err)
 				return
 			}
-			is_Dir := info.IsDir()
-			if !is_Dir {
+			isDir := info.IsDir()
+			if !isDir {
 				fmt.Println("目标不是目录")
 				return
 			}
-			fmt.Printf("准备扫描目录:%v\n", cleanPath)
+			count := 0
+			var bytesSum int64 = 0
+			walkDirErr := filepath.WalkDir(cleanPath, func(path string, d fs.DirEntry, err error) error {
+				if err != nil {
+					return err
+				}
+
+				fileInfo, err := d.Info()
+				if err != nil {
+					return err
+				}
+				sz := fileInfo.Size()
+
+				relPath, err := filepath.Rel(cleanPath, path)
+				if err != nil {
+					return err
+				}
+				if fileInfo.Mode().IsRegular() {
+					fmt.Printf("%v %v bytes\n", relPath, sz)
+					count++
+					bytesSum += sz
+				}
+				return nil
+			})
+			if walkDirErr != nil {
+				fmt.Printf("扫描目录失败, 错误信息:%v\n", walkDirErr)
+				return
+			}
+			fmt.Printf("文件数量: %v\n", count)
+			fmt.Printf("总大小: %v bytes\n", bytesSum)
+
 		} else {
 			fmt.Println("请输入正确的功能参数！")
 		}
