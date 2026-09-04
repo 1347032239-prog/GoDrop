4a8962ddb9faed5e579104a531c55eafb5ac1367
xantarescpp <1347032239@qq.com>
2026-09-04 18:52:40 +0800

    feat: read and display JSON indexes

--- index.go
+++ index.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 )
 
@@ -24,4 +25,32 @@ func writeIndex(path string, result ScanResult) error {
 	}
 
 	return nil
+}
+
+func readIndex(path string) (ScanResult, error) {
+	var result ScanResult
+	file, err := os.Open(path)
+	if err != nil {
+		return result, err
+	}
+
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	derr := decoder.Decode(&result)
+	if derr != nil {
+		return result, derr
+	}
+
+	return result, nil
+
+}
+
+func printScanResult(result ScanResult) {
+	length := len(result.Files)
+	for i := 0; i < length; i++ {
+		fmt.Printf("%v %v bytes\n", result.Files[i].Path, result.Files[i].Size)
+	}
+	fmt.Printf("文件数量: %v\n", length)
+	fmt.Printf("总大小: %v bytes\n", result.TotalSize)
 }
